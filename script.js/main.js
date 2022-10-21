0631b4732b6a64ce0f00c2ae7147330a66f7146e
simeonsimeon <lingakumardae@gmail.com>
2022-10-21 17:57:50 -0600

    add

--- script.js/main.js
+++ script.js/main.js
@@ -1,12 +1,23 @@
 let Show = document.getElementById("bright");
+let click = document.getElementById("Click");
+let head = document.getElementById("Head");
+let Hero = document.getElementById("Hero")
  let isActive = false
  function show(){
    isActive = !isActive;
    if(isActive){
      Show.style.display = "block";
+    //  if(click.classList.contains("icofont-navigation-menu")){
+    //   click.classList.contains.replace("icofont-navigation-menu,icofont-simple-down");
+    //  }
+    //  document.body.classList.add("Bdy");
+    //  head.classList.add("blur");
+    //  hero.classList.add("blur");
     
    }else{
     Show.style.display = "none";
+    // hero.classList.remove("blur")
+    // document.body.classList.remove("Bdy");
    }
  }
- document.getElementById("Click").addEventListener("click",show);
+ click.addEventListener("click",show);
